25f53a96e60df62ce73db6bc36c4064251bcb923
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-26 15:44:02 -0400

    feat: Add class to represent menu items

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,3 +1,25 @@
+class MenuItem {
+    #item;
+    #image;
+    #description
+    #price;
+
+    constructor(item, image, description, price) {
+        this.#item = item;
+        this.#image = image;
+        this.#description = description;
+        this.#price = price;
+    }
+
+    get item() { return this.#item; }
+    get image() { return this.#image; }
+    get description() { return this.#description; }
+    get price() { return this.#price; }
+}
+
 export function createMenu() {
-    
+    const menuDiv = document.createElement('div');
+    menuDiv.classList.add('menu-container');
+
+    return menuDiv;
 }
