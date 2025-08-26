84be3e8d8848cb319a994b73f1a3ba4d7de3df1d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-26 14:32:41 -0400

    refactor: Add module to index.js to handle page load events

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import {createHomePage} from "./modules/home";
 const contentDiv = document.getElementById('content');
 contentDiv.appendChild(createHomePage());
 
-console.log('Welcome to the Restaurant!');
+const tabHandler = (function() {
+    const contentDiv = document.getElementById('content');
+
+    /**Clear the content container, and then add the home page content.
+     * This is done upon module initialization as the initial page load
+     */
+    contentDiv.innerHTML = '';
+    contentDiv.appendChild(createHomePage());
+})();
