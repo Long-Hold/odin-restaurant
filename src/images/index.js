5534bd3fcd4ba1e31e35e2b377a0df7526999f21
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-27 14:13:27 -0400

    feat: Add docstring to module

--- src/images/index.js
+++ src/images/index.js
@@ -4,6 +4,14 @@ import mousse from './mousse.png';
 import mushroompotato from './mushroompotato.png';
 import sirloin from './sirloin.png';
 
+/**This module gathers all the file paths and exports them
+ * in an object for easy use.
+ * 
+ * Rather than importing each image individually in the menu.js module,
+ * we can instead import this object. Anytime an image is added or removed,
+ * only this module needs to be updated.
+ */
+
 export const foodImage = {
     buratta,
     filetmignon,
