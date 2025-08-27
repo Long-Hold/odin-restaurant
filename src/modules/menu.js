8080608c0bafe584a8d5831f9bd9b9f0fed0ab1d
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-27 13:50:12 -0400

    feat: Store menu objects in an object

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -19,9 +19,17 @@ class MenuItem {
     get price() { return this.#price; }
 }
 
+const menuItems = {
+    burratta: new MenuItem('Burratta', foodImage.buratta, 'A mystical twist on the classic.', '$14.50'),
+    filetMignon: new MenuItem('Filet Mignon', foodImage.filetmignon, 'A classic cut of prime beef.', '$25.50'),
+    mousse: new MenuItem('Chocolate Mousse', foodImage.mousse, 'A rich chocolate treat served with raspberry glaze.', '$20.00'),
+    mushroomParadise: new MenuItem('Mushroom Paradise', foodImage.mushroompotato, 'A fully vegan dish served with rich mushrooms and potatos.', '$18.50'),
+    sirloin: new MenuItem('Sirloin', foodImage.sirloin, 'A classic sirloin served with red wine sauce and grilled veggies.', '$20.00'),
+};
+
 export function createMenu() {
     const menuDiv = document.createElement('div');
     menuDiv.classList.add('menu-container');
-    
+
     return menuDiv;
 }
