4aa5d8a2e865eb4009de19944f9448c9002a305f
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-27 15:15:59 -0400

    feat: Create module that draws the about page

---
+++ src/modules/about.js
@@ -0,0 +1,8 @@
+export function createAbout() {
+    const aboutDiv = document.createElement('div');
+    aboutDiv.classList.add('about-container');
+
+    const storySection = document.createElement('section');
+
+    return aboutDiv;
+}
