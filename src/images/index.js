b8382a96ec0a66c60b1779614ed9d1ed3e5c277c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2025-08-27 12:14:31 -0400

    feat: Add module to export image paths

---
+++ src/images/index.js
@@ -0,0 +1,13 @@
+import buratta from './burratta.png';
+import filetmignon from  './filetmignon.png';
+import mousse from './mousse.png';
+import mushroompotato from './mushroompotato.png';
+import sirloin from './sirloin.png';
+
+export const foodImage = {
+    buratta,
+    filetmignon,
+    mousse,
+    mushroompotato,
+    sirloin,
+};
